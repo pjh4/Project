21cbf4f0199c0efd596dcd2e8863b286dae0202c
pjh4 <pjh4@njit.edu>
2016-04-30 17:00:18 -0400

    Normalization 2

--- Normalization (copy).sql
+++ Normalization (copy).sql
@@ -1,11 +1,31 @@
+-- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: permissions
+-- ------------------------------------------------------
+-- Server version	5.5.47-0ubuntu0.14.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `groups`
+--
+
 DROP TABLE IF EXISTS `groups`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `groups` (
-  `groupID` varchar(32) DEFAULT NULL,
-  `userID` int (50) DEFAULT NULL,
-  `permissionLevel` int(100) DEFAULT NULL,
-  PRIMARY KEY (`groupID`)
+  `username` varchar(50) DEFAULT NULL,
+  `loginType` enum('Admin','User','Guest') DEFAULT NULL,
+  `permissionLevel` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -15,7 +35,7 @@ CREATE TABLE `groups` (
 
 LOCK TABLES `groups` WRITE;
 /*!40000 ALTER TABLE `groups` DISABLE KEYS */;
-INSERT INTO `groups` VALUES ('Ad1','1',100),('Ad2','2',100),('Us1','5',200),('Us2','15',200),('Gu1','16',300),('Gu2','24',300);
+INSERT INTO `groups` VALUES ('root','Admin',1),('Admin','Admin',1),('TK4210','User',2),('User','User',2),('Pikachu','Guest',3),('MegaMan','Guest',3);
 /*!40000 ALTER TABLE `groups` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -56,7 +76,6 @@ CREATE TABLE `server` (
   `serverName` varchar(32) DEFAULT NULL,
   `IPaddress` varchar(15) DEFAULT NULL,
   `serverDesc` varchar(255) DEFAULT NULL,
-  `OwnerID` int(11) DEFAULT NULL,
   PRIMARY KEY (`serverID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -67,6 +86,17 @@ CREATE TABLE `server` (
 
 LOCK TABLES `server` WRITE;
 /*!40000 ALTER TABLE `server` DISABLE KEYS */;
-INSERT INTO `server` VALUES (1,'TK4210s server','173.50.12.11:10','This is my server','5'),(2,'Onett','127.0.0.1','The homeliest server','2'),(3,'Test','167.14.121.1','This is a test server','15'),(4,'Test','18.92.9.0','This is a test server','24');
+INSERT INTO `server` VALUES (1,'TK4210s server','173.50.12.11:10','This is my server'),(2,'Onett','127.0.0.1','The homeliest server'),(3,'Test','167.14.121.1','This is a test server'),(4,'Test','18.92.9.0','This is a test server');
 /*!40000 ALTER TABLE `server` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-21 22:37:21
