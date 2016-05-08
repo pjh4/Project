fe797f5389be37d456589655dbc49bb335d86cdd
pjh4 <pjh4@njit.edu>
2016-05-08 16:35:42 -0400

    Mongo server

--- MongoDB.php
+++ MongoDB.php
@@ -1,34 +1,40 @@
 <?php
-// connect
-$uri = "mongodb://pjh4:pjh4njit@ds011902.mlab.com:11902/permissions";
+  try 
+  {
+    $uri = "mongodb://pjh4:pjh4njit@ds011902.mlab.com:11902/permissions";
+    $m = new MongoClient($uri);
+    echo "Database connected sucessfully".PHP_EOL;
+    $collection = $m->permissions->server;
+    echo "Collection connected sucessfully".PHP_EOL;
+    
+    $serverID = '1';
 
+    $Query = array('serverID' => $serverID);
 
-try {
-	$m = new MongoClient($uri);	
-	echo "Collection Selected sucessfully".PHP_EOL;
-}
-catch (MongoConnectionException $e) {
-	die('Error connecting to MongoDB server'.PHP_EOL);
-} 
+    $cursor = $collection->find($Query);
 
-// select a database
-$db = $m->permissions;
-
-// select a collection
-$collection = $db->login;
-
-$Username = 'Pikachu';
-
-$Query = array('username' => $Username);
-
-$cursor = $collection->find($Query);
-
-foreach ($cursor as $id) 
-{
-  echo $id["username"] . "\n";
-}
-
-
-//close connection
-$m->close();
+    foreach ($cursor as $id) 
+    {
+      echo $id["serverID"] . "\n" . $id["serverName"] . "\n" . $id["IPaddress"]
+      . "\n" . $id["serverDesc"] . "\n" . $id["OwnerID"] . "\n".PHP_EOL;
+    }    
+    
+    /* Show All Users
+    $cursor = $collection->find();
+    foreach ($cursor as $id => $value) 
+    {
+      echo "$id: ";
+      var_dump( $value );	
+    }
+    */
+  }
+  
+  catch (MongoConnectionException $e) 
+  {
+    die('Error connecting to MongoDB server');
+  } catch (MongoConnectionException $e) 
+  { 
+    die('Error: ' . $e->getMessage());
+  }
 ?>
+
